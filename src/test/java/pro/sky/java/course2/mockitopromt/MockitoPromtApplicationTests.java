b5ef460d56b83e5bcd5880ad62ebc123c3055be8
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-25 09:17:17 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/mockitopromt/MockitoPromtApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.mockitopromt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoPromtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
